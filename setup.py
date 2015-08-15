4f48633533067451f8b4fe0021227b3589d18d34
Gregor Kališnik <gregor@unimatrix.si>
2015-08-15 20:00:38 +0200

    Released new patch release v0.1.5.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
   name = 'Flask-phpBB3',
-  version = '0.1.4',
+  version = '0.1.5',
   url = 'https://github.com/MasterMind2k/flask-phpbb3',
   license = 'BSD',
   author = 'Gregor Kalisnik',
