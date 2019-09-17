f03e8133e3a8f3cc889ed71e2ee64562e771abe0
Gregor <rus.gregor@gmail.com>
2019-09-17 02:48:49 +0200

    changes in tests

--- tests/integration/test_extension.py
+++ tests/integration/test_extension.py
@@ -69,79 +69,84 @@ class TestFetch(base.TestWithDatabase):
             privilege = self.app.phpbb3.fetch_acl_options(skip=skip, limit=1)
             self.assertEqual((skip, privilege), expected_privileges[skip])
 
-    def test_fetch_global_topics(self):
+    def test_fetch_global_topics_positive(self):
         # type: () -> None
         base._create_global_topics(self.cursor)
 
         expected_topics = [(0, [{
-                                'topic_id': 0,
-                                'forum_id': 0,
-                                'topic_title': 'topic title 0',
-                                'topic_time': 10,
-                                'topic_first_poster_name': 'name',
-                                'post_subject': 'topic one',
-                                'post_text': 'hello'}]),
+            'topic_id': 0,
+            'forum_id': 0,
+            'topic_title': 'topic title 0',
+            'topic_time': 10,
+            'topic_first_poster_name': 'name',
+            'post_subject': 'topic one',
+            'post_text': 'hello'}]),
 
                            (1, [{
-                                'topic_id': 1,
-                                'forum_id': 0,
-                                'topic_title': 'topic title 1',
-                                'topic_time': 13,
-                                'topic_first_poster_name': 'second poster',
-                                'post_subject': 'topic two',
-                                'post_text': 'hello world'}]),
-                           (2, [{
-                                'topic_id': 2,
-                                'forum_id': 0,
-                                'topic_title': 'topic title 2',
-                                'topic_time': 200,
-                                'topic_first_poster_name': 'post it',
-                                'post_subject': 'topic three',
-                                'post_text': 'hello hello'}]),
-                           (3, [{
-                                'topic_id': 3,
-                                'forum_id': 0,
-                                'topic_title': 'topic title 3',
-                                'topic_time': 256,
-                                'topic_first_poster_name': 'posted it',
-                                'post_subject': 'topic three',
-                                'post_text': 'hello times four'}]),
-                           (4, [{
-                                'topic_id': 4,
-                                'forum_id': 2,
-                                'topic_title': 'topic missing',
-                                'topic_time': 666,
-                                'topic_first_poster_name': 'no forum',
-                                'post_subject': 'topic yes, forum no',
-                                'post_text': 'test case'}]),
-                           (5, [{
-                               'topic_id': 5,
+                               'topic_id': 1,
                                'forum_id': 0,
-                               'topic_title': 'topic missing',
-                               'topic_time': 777,
-                               'topic_first_poster_name': 'not existent forum',
-                               'post_subject': 'topic yes, forum no',
-                               'post_text': 'test case'}]),
-                           (6, [{
-                               'unexpected_column': 5,
-                               'random_column': 0,
-                               'topic_title': 'topic missing',
-                               'topic_time': 777,
-                               'do_not_change_all': 'not existent forum',
-                               'post_subject': 'topic yes, forum no',
-                               'post_text': 'test case'}]),
-                           (7, [1, 2, 3])
+                               'topic_title': 'topic title 1',
+                               'topic_time': 13,
+                               'topic_first_poster_name': 'second poster',
+                               'post_subject': 'topic two',
+                               'post_text': 'hello world'}]),
+                           (2, [{
+                               'topic_id': 2,
+                               'forum_id': 0,
+                               'topic_title': 'topic title 2',
+                               'topic_time': 200,
+                               'topic_first_poster_name': 'post it',
+                               'post_subject': 'topic three',
+                               'post_text': 'hello hello'}]),
+                           (3, [{
+                               'topic_id': 3,
+                               'forum_id': 0,
+                               'topic_title': 'topic title 3',
+                               'topic_time': 256,
+                               'topic_first_poster_name': 'posted it',
+                               'post_subject': 'topic three',
+                               'post_text': 'hello times four'}])
                            ]
 
-        for skip in range(0, 7):
+        for skip in range(0, 3):
             topic = self.app.phpbb3.fetch_global_topics(
                 skip=skip,
                 limit=1,
                 forum_id=0)
-            if skip > 3:
-                self.assertNotEqual((skip, topic), expected_topics[skip])
-            else:
-                self.assertEqual((skip, topic), expected_topics[skip])
+
+            self.assertEqual((skip, topic), expected_topics[skip])
+
+    def test_fetch_global_topics_topic_not_existing(self):
+        # type: () -> None
+        base._create_global_topics(self.cursor)
+        skip = 4
+        topic = self.app.phpbb3.fetch_global_topics(
+            skip=skip,
+            limit=1,
+            forum_id=0)
+
+        self.assertTrue(topic == [])
+
+    def test_fetch_global_topics_forum_not_existing(self):
+        # type () -> None
+        base._create_global_topics(self.cursor)
+        skip = 5
+        topic = self.app.phpbb3.fetch_global_topics(
+            skip=skip,
+            limit=1,
+            forum_id=1)
+
+        self.assertTrue(topic == [])
+
+    def test_fetch_global_topics_not_global_topic(self):
+        # type () -> None
+        base._create_global_topics(self.cursor)
+        skip=6
+        topic = self.app.phpbb3.fetch_global_topics(
+            skip=skip,
+            limit=1,
+            forum_id=0)
+        self.assertTrue(topic == [])
 
 
 class TestSession(base.TestWithDatabase):
